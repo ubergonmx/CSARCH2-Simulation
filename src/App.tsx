42298e2e590ddc677156b77de10d28d3df3f063e
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-04-06 18:51:19 +0800

    * applied tailwind and changed layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,13 @@ import {convert} from './dpdsolver'
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [inputDecimal, setInputDecimal] = useState("");
   const [binary, setBinary] = useState("");
 
   function handleBinaryConvert(){
-    const string = "431";
-    setBinary(convert(string));
-
-    // const result = new Decimal(418);
-    // const bin = twosComplement(result.toBinary().replace('0b', ''));
-    // console.log(bin);
-    // console.log(signExtend(bin, 10));
-    // setBinary(signExtend(bin, 10));
+    console.log(inputDecimal);
+    console.log(convert(inputDecimal));
+    setBinary(convert(inputDecimal));
   }
 
   // create a function that sign extends a binary string
@@ -58,22 +53,33 @@ function App() {
 
   return (
     <div className="App">
-      <h1>IEEE-754 Decimal-64 floating-point converter</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">IEEE-754 Decimal-64 floating-point converter</h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Simply enter the corresponding inputs, and the website will generate its equivalent in the 64-bit floating-point format, along with additional information about its sign, exponent, and mantissa.</p>
+          </div>
+          <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:px-0 items-end sm:space-x-4 sm:space-y-0 space-y-4">
+            <div className="relative sm:mb-0 flex-grow w-full">
+              <label htmlFor="full-name" className="leading-7 text-sm text-gray-400">Decimal (up to 16 digit) - Currently DPD hehe</label>
+              <input onChange={(e) => setInputDecimal(e.target.value)} type="text" id="full-name" name="full-name" className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            </div>
+            <div className="relative sm:mb-0 flex-grow w-full">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-400">Exponent</label>
+              <input type="email" id="email" name="email" className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            </div>
+            <button onClick={handleBinaryConvert} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Compute</button>
+          </div>
 
-        <button onClick={handleBinaryConvert}>
-          dpd is {binary}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          { binary !== "" &&
+          <>
+            <div className="flex flex-col text-center w-full mt-12">
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Output: {binary}</p>
+            </div>
+          </>
+          }
+        </div>
+      </section>
     </div>
   )
 }
